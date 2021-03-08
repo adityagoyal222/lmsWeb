e429e480a615fc8ca5599445797acd09b2304340
Aditya Goyal <goyaladitya85@gmail.com>
2021-03-08 19:51:34 +0545

    created UserSerializer and login, register viewset

--- courses/serializers.py
+++ courses/serializers.py
@@ -4,6 +4,16 @@ from .models import Course, Enrollment
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Course
         fields = "__all__"
+        
+
+# class EnrollmentSerializer(serializers.ModelSerializer):
+#     course = CourseSerializer(many=True)
+
+
+#     class Meta:
+#         model = Enrollment
+#         fields = ('course', 'student')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+
+from .models import User
+from .serializers import UserSerializer
+
 
 # Create your views here.
+@api_view(['POST'])
+def create_auth(request):
+    serialized = UserSerializer(data=request.DATA)
+    if serialized.is_valid():
+        User.objects.create_user(
+            serialized.init_data['email'],
+            serialized.init_data['username'],
+            serialized.init_data['password']
+        )
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({"error": "Login failed"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({"token": token.key})
