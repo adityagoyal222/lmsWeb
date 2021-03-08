037d71a0f87286ae08f3a8cffeaf5f28a996e36c
Aditya Goyal <goyaladitya85@gmail.com>
2021-03-08 18:13:51 +0545

    initialized courses app

--- lmsWeb/settings.py
+++ lmsWeb/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'bootstrap',
+    'rest_framework',
+
+    'assignments',
+    'courses'
 ]
 
 MIDDLEWARE = [
