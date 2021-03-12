839e154592479b33001b09d630b48f7f79f3f8b0
Aditya Goyal <goyaladitya85@gmail.com>
2021-03-12 18:50:47 +0545

    created AssingmentViewSet

--- assignments/views.py
+++ assignments/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
+
+from .serializers import AssignmentSerializer, SubmitAssignmentSerializer
+from .models import Assignment, SubmitAssignment
+from users.permissions import StudentPermission, TeacherPermission
 
 # Create your views here.
+class AssignmentViewSet(ModelViewSet):
+    serializer_class = AssignmentSerializer
+    queryset = Assignment.objects.all()
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [TeacherPermission]
+
+        return [permission() for permission in permission_classes]
