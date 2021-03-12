3ea2a359f330f57ecd0d653b4e826924fa936d40
Aditya Goyal <goyaladitya85@gmail.com>
2021-03-12 18:44:39 +0545

    created SubmitAssignmentSerializer

--- assignments/serializers.py
+++ assignments/serializers.py
@@ -8,4 +8,12 @@ class AssignmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Assignment
         fields = ('id', 'assignment_name', 'assignment_description', 'start_date', 'due_date', 'course')
-    
+
+
+class SubmitAssignmentSerializer(serializers.ModelSerializer):
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = SubmitAssignment
+        fields = ('id', 'author', 'topic', 'submission_text', 'submission_file', 'submitted_date', 'assignment_ques', 'graded', 'grade')
+        
